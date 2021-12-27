5499b376c313c1f800a9172699a0d444af5fc240
ApexHC <ethan.hr@outlook.com>
2021-12-27 10:52:57 -0500

    Created project with taskModel DataModel

---
+++ Program.cs
@@ -0,0 +1,7 @@
+//Need to create new instance of TaskModel class
+using ExampleServer.Data;
+TaskModel firstTask = new TaskModel("This is my first task");
+TaskModel secondTask = new("This is my second task.");
+
+Console.WriteLine($"First task id: {firstTask.Id");
+Console.WriteLine($"Second task id; {secontTask.Id});
